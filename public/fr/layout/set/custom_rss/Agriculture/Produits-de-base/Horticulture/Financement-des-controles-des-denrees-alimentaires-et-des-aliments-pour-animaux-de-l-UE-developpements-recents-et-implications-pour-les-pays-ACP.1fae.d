53d8f2940881a0d9a0710e529d911a4adb77e081
Martijn <martijn.scholten@nordcloud.com>
2018-08-22 11:35:48 +0200

    update static httrack content

---
+++ public/fr/layout/set/custom_rss/Agriculture/Produits-de-base/Horticulture/Financement-des-controles-des-denrees-alimentaires-et-des-aliments-pour-animaux-de-l-UE-developpements-recents-et-implications-pour-les-pays-ACP.1fae.d
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+	<title>Agritrade - CTA</title>
+	<id>http://agritrade.cta.int/fr/layout/set/custom_rss/Agriculture/Produits-de-base/Horticulture/Financement-des-controles-des-denrees-alimentaires-et-des-aliments-pour-animaux-de-l-UE-developpements-recents-et-implications-pour-les-pays-ACP</id>
+	<link rel="self" href="http://agritrade.cta.int/fr/layout/set/custom_rss/Agriculture/Produits-de-base/Horticulture/Financement-des-controles-des-denrees-alimentaires-et-des-aliments-pour-animaux-de-l-UE-developpements-recents-et-implications-pour-les-pays-ACP" />
+	<author>
+		<name>Agritrade - CTA</name>
+	</author>
+	<updated>2018-08-22T01:39:32Z</updated>
+
+	
+		
+
+
+	
+																																									
+														
+																																																		
+																																
+																				
+		
+		
+	
+																																																	
+					
+
+
+		
+	
+	
+	
+
+			
+	
+						
+	</feed>
